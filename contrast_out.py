55f7351465d9f8363558f88a6da983f3e0fd867f
Wojciech Ziolek <wojciech.ziolek@cern.ch>
2013-10-29 14:15:27 +0100

    SCOAP3: tmp

--- contrast_out.py
+++ contrast_out.py
@@ -1,14 +1,16 @@
+from __future__ import division
 from ftplib import FTP
 from xml.dom.minidom import parseString
 from contrast_out_config import *
-
+from functools import partial
+import sys
 
 class ContrastOutConnector(object):
     def __init__(self):
         self.ftp = None
-        self.ready_file_list = []
+        self.files_list = []
         self.ready_xml = []
-        self.a = ''
+        self.a = {}
 
     def connect(self):
         """Logs into the specified ftp server and returns connector."""
@@ -17,17 +19,40 @@ class ContrastOutConnector(object):
 
     def _get_file_listing(self, phrase=None):
         if phrase:
-            self.ready_file_list = filter(lambda x: phrase in x, self.ftp.nlst())
+            self.files_list = filter(lambda x: phrase in x, self.ftp.nlst())
         else:
-            self.ready_file_list = self.ftp.nlst()
+            self.files_list = self.ftp.nlst()
 
-    def read_ready_list(self):
-        for f in self.ready_file_list:
-            self.ftp.retrlines('RETR %s' % (f,), self._add_to_a)
-        print self.a
-            #self.ready_xml.append(parseString(a))
+    def _read_file_listing(self):
+        # Prints stuff
+        print >> sys.stdout, "Reading %i files to retrieve packages." % (len(self.files_list))
+        # Create progrss bar
+        p_bar = self._progress_bar(len(self.files_list))
+        # Print stuff
+        sys.stdout.write(p_bar.next())
+        sys.stdout.flush()
+        for f in self.files_list:
+            self.a[f] = ""
+            self.ftp.retrlines('RETR %s' % (f,), partial(self._add_to_a, f))
+            self.ready_xml.append(parseString(self.a[f]))
+            # Print stuff
+            sys.stdout.write(p_bar.next())
+            sys.stdout.flush()
+        # Print stuff
+        print >> sys.stdout, "\nDone."
 
-    ## There should be way to change that but all the other things I've tried 
+    ## There should be way to change that but all the other things I've tried
     ## failed :(
-    def _add_to_a(self, x):
-        self.a += x
+    def _add_to_a(self, key, val):
+        self.a[key] += val
+
+    def run(self):
+        self.connect()
+        self._get_file_listing('.ready')
+        self._read_file_listing()
+
+    def _progress_bar(self, n):
+        num = 0
+        while num <= n:
+            yield "\r%i%% %s%s" %(num/n*100,"="*num,'-'*(n-num))
+            num += 1
