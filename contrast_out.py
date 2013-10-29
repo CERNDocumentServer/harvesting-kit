dbbd5af5ab3869a9da209832edb6af291fcc45e1
Wojciech Ziolek <wojciech.ziolek@cern.ch>
2013-10-29 17:42:23 +0100

    SCAOP3: Downloadind tar files

--- contrast_out.py
+++ contrast_out.py
@@ -1,9 +1,16 @@
 from __future__ import division
-from ftplib import FTP
-from xml.dom.minidom import parseString
-from contrast_out_config import *
-from functools import partial
+
 import sys
+
+from functools import partial
+from ftplib import FTP
+from os.path import join
+from tempfile import mkdtemp
+from xml.dom.minidom import parseString
+
+from scoap3utils import xml_to_text
+from contrast_out_config import *
+from invenio.config import CFG_TMPSHAREDDIR
 
 class ContrastOutConnector(object):
     def __init__(self):
@@ -11,6 +18,7 @@ class ContrastOutConnector(object):
         self.files_list = []
         self.ready_xml = []
         self.a = {}
+        self.retrieved_packages = []
 
     def connect(self):
         """Logs into the specified ftp server and returns connector."""
@@ -25,12 +33,13 @@ class ContrastOutConnector(object):
 
     def _read_file_listing(self):
         # Prints stuff
-        print >> sys.stdout, "Reading %i files to retrieve packages." % (len(self.files_list))
+        print >> sys.stdout, "\nReading %i files." % (len(self.files_list))
         # Create progrss bar
         p_bar = self._progress_bar(len(self.files_list))
         # Print stuff
         sys.stdout.write(p_bar.next())
         sys.stdout.flush()
+
         for f in self.files_list:
             self.a[f] = ""
             self.ftp.retrlines('RETR %s' % (f,), partial(self._add_to_a, f))
@@ -38,8 +47,43 @@ class ContrastOutConnector(object):
             # Print stuff
             sys.stdout.write(p_bar.next())
             sys.stdout.flush()
+
+    def _get_packages(self):
+        # Prints stuff
+        print >> sys.stdout, "\nRetrieving packages."
+        # Create progrss bar
+        p_bar = self._progress_bar(len(self.files_list))
         # Print stuff
-        print >> sys.stdout, "\nDone."
+        sys.stdout.write(p_bar.next())
+        sys.stdout.flush()
+
+        for pack in self.ready_xml:
+            filenames = pack.getElementsByTagName('filename')
+            for filename in filenames:
+                self.retrieved_packages.append(xml_to_text(filename))
+             # Print stuff
+            sys.stdout.write(p_bar.next())
+            sys.stdout.flush()
+
+    def _download_tars(self):
+        self.path_tar = mkdtemp(prefix="scoap3_tar", dir=CFG_TMPSHAREDDIR)
+        
+        # Prints stuff
+        print >> sys.stdout, "\nDownloading %i tar packages." \
+            % (len(self.retrieved_packages))
+        # Create progrss bar
+        p_bar = self._progress_bar(len(self.files_list))
+        # Print stuff
+        sys.stdout.write(p_bar.next())
+        sys.stdout.flush()
+
+        for filename in self.retrieved_packages:
+            tar_file = open(join(self.path_tar,filename), 'wb')
+            self.ftp.retrbinary('RETR %s' % filename, tar_file.write)
+            tar_file.close()
+            # Print stuff
+            sys.stdout.write(p_bar.next())
+            sys.stdout.flush()
 
     ## There should be way to change that but all the other things I've tried
     ## failed :(
@@ -50,6 +94,8 @@ class ContrastOutConnector(object):
         self.connect()
         self._get_file_listing('.ready')
         self._read_file_listing()
+        self._get_packages()
+        self._download_tars()
 
     def _progress_bar(self, n):
         num = 0
