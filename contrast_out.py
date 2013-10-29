358d5d8417b7c6373bee7c1b26a8a00d8c4d5e21
Wojciech Ziolek <wojciech.ziolek@cern.ch>
2013-10-29 23:56:13 +0100

    SCOAP3: gets all correct xml and pdf URL from CONTRAST-out protocol.
    * Adds reading .ready files from FTP server.
    * Adds downloading tar packages from server.
    * Adds checking MD5 of tar packages.
    * Adds extracting packages to TMP directory.
    * Adds getting xml and pdf URLs from dataset files.

--- contrast_out.py
+++ contrast_out.py
@@ -5,12 +5,13 @@ import sys
 from functools import partial
 from ftplib import FTP
 from os.path import join
+from tarfile import TarFile
 from tempfile import mkdtemp
-from xml.dom.minidom import parseString
+from xml.dom.minidom import parseString, parse
 
-from scoap3utils import xml_to_text
 from contrast_out_config import *
 from invenio.config import CFG_TMPSHAREDDIR
+from scoap3utils import xml_to_text
 
 class ContrastOutConnector(object):
     def __init__(self):
@@ -18,7 +19,12 @@ class ContrastOutConnector(object):
         self.files_list = []
         self.ready_xml = []
         self.a = {}
-        self.retrieved_packages = []
+        self.retrieved_packages = {}
+        self.retrieved_packages_unpacked = []
+        self.path = None
+        self.path_tar = None
+        self.retrieved_packages_unpacked = []
+        self.found_articles = []
 
     def connect(self):
         """Logs into the specified ftp server and returns connector."""
@@ -50,7 +56,7 @@ class ContrastOutConnector(object):
 
     def _get_packages(self):
         # Prints stuff
-        print >> sys.stdout, "\nRetrieving packages."
+        print >> sys.stdout, "\nRetrieving packages names."
         # Create progrss bar
         p_bar = self._progress_bar(len(self.files_list))
         # Print stuff
@@ -58,29 +64,76 @@ class ContrastOutConnector(object):
         sys.stdout.flush()
 
         for pack in self.ready_xml:
-            filenames = pack.getElementsByTagName('filename')
-            for filename in filenames:
-                self.retrieved_packages.append(xml_to_text(filename))
+            package_file = pack.getElementsByTagName('dataset-package-file')
+            for pf in package_file:
+                filename = pf.getElementsByTagName('filename')[0]
+                md5_val = pf.getElementsByTagName('md5')[0]
+                self.retrieved_packages[xml_to_text(filename)] = xml_to_text(md5_val)
              # Print stuff
             sys.stdout.write(p_bar.next())
             sys.stdout.flush()
 
     def _download_tars(self):
         self.path_tar = mkdtemp(prefix="scoap3_tar", dir=CFG_TMPSHAREDDIR)
-        
+
         # Prints stuff
         print >> sys.stdout, "\nDownloading %i tar packages." \
-            % (len(self.retrieved_packages))
+                 % (len(self.retrieved_packages))
         # Create progrss bar
         p_bar = self._progress_bar(len(self.files_list))
         # Print stuff
         sys.stdout.write(p_bar.next())
         sys.stdout.flush()
 
-        for filename in self.retrieved_packages:
-            tar_file = open(join(self.path_tar,filename), 'wb')
-            self.ftp.retrbinary('RETR %s' % filename, tar_file.write)
-            tar_file.close()
+        for filename in self.retrieved_packages.iterkeys():
+            try:
+                unpack_path = join(self.path_tar, filename)
+                self.retrieved_packages_unpacked.append(unpack_path)
+                tar_file = open(unpack_path, 'wb')
+                self.ftp.retrbinary('RETR %s' % filename, tar_file.write)
+                tar_file.close()
+            except:
+                print >> sys.stdout, "\nError downloading %s file!" % (filename,)
+                print >> sys.stdout, sys.exc_info()
+            # Print stuff
+            sys.stdout.write(p_bar.next())
+            sys.stdout.flush()
+
+    def _check_md5(self):
+        import hashlib
+
+        for filename, md5 in self.retrieved_packages.iteritems():
+            our_md5 = hashlib.md5(open(join(self.path_tar, filename)).read()).hexdigest()
+            if our_md5 != md5:
+                print >> sys.stdout, "Error in MD5 of %s" % (filename,)
+                print >> sys.stdout, "oryginal: %s, ours: %s" % (md5, our_md5)
+
+    def _extract_packages(self):
+        """
+        Extract a package in a new temporary directory.
+        """
+        self.path_unpacked = mkdtemp(prefix="scoap3_package", dir=CFG_TMPSHAREDDIR)
+        for path in self.retrieved_packages_unpacked:
+            TarFile.open(path).extractall(self.path_unpacked)
+
+    def _get_metadata_and_fulltex_dir(self):
+        # Prints stuff
+        print >> sys.stdout, "\nRetrieving jurnal items directories."
+        # Create progrss bar
+        p_bar = self._progress_bar(len(self.files_list))
+        # Print stuff
+        sys.stdout.write(p_bar.next())
+        sys.stdout.flush()
+
+        for name in self.files_list:
+            dataset_link = join(self.path_unpacked, name.split('.')[0], 'dataset.xml')
+
+            dataset_xml = parse(dataset_link)
+            jurnal_items = dataset_xml.getElementsByTagName('journal-item')
+            for jurnal_item in jurnal_items:
+                xml_pathname = join(self.path_unpacked, name.split('.')[0], xml_to_text(jurnal_item.getElementsByTagName('ml')[0].getElementsByTagName('pathname')[0]))
+                pdf_pathname = join(self.path_unpacked, name.split('.')[0], xml_to_text(jurnal_item.getElementsByTagName('web-pdf')[0].getElementsByTagName('pathname')[0]))
+                self.found_articles.append(dict(xml=xml_pathname, pdf=pdf_pathname))
             # Print stuff
             sys.stdout.write(p_bar.next())
             sys.stdout.flush()
@@ -96,9 +149,13 @@ class ContrastOutConnector(object):
         self._read_file_listing()
         self._get_packages()
         self._download_tars()
+        self._check_md5()
+        self._extract_packages()
+        self._get_metadata_and_fulltex_dir()
+        print self.found_articles
 
     def _progress_bar(self, n):
         num = 0
         while num <= n:
-            yield "\r%i%% %s%s" %(num/n*100,"="*num,'-'*(n-num))
+            yield "\r%i%% [%s%s]" % (num/n*100, "="*num, '.'*(n-num))
             num += 1
