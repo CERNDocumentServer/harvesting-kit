b7595044f40bd29ac8d6c798234dac99a0d4a633
Wojciech Ziolek <wojciech.ziolek@cern.ch>
2013-11-18 18:12:41 +0100

    tmp

--- springer_package.py
+++ springer_package.py
@@ -1,15 +1,21 @@
 import sys
+import time
 import traceback
 
 from datetime import datetime
-from invenio.config import (CFG_LOGDIR, CFG_SPRINGER_DOWNLOADDIR, CFG_ETCDIR)
+from functools import partial
+from invenio.bibrecord import record_add_field, record_xml_output
+from invenio.bibtask import task_low_level_submission
+from invenio.config import (CFG_LOGDIR, CFG_SPRINGER_DOWNLOADDIR, CFG_ETCDIR,
+                            CFG_TMPSHAREDDIR)
 from invenio.errorlib import register_exception
 from invenio.shellutils import run_shell_command
-from os import listdir, rename
+from os import listdir, rename, fdopen
 from os.path import join, walk, exists
-from scoap3utils import create_logger
+from scoap3utils import (create_logger, get_value_in_tag, xml_to_text)
 from tarfile import TarFile
-from tempfile import mkdtemp
+from tempfile import mkdtemp, mkstemp
+from xml.dom.minidom import parse
 from zipfile import ZipFile
 CFG_SCOAP3DTDS_PATH = join(CFG_ETCDIR, 'scoap3dtds')
 
@@ -31,6 +37,7 @@ class SpringerPackage(object):
     def __init__(self, package_name=None, path=None):
         self.package_name = package_name
         self.path = path
+        self._dois = []
         print >> sys.stderr, "aa"
         self.logger = create_logger(join(CFG_LOGDIR, 'springer_harvesting_'+str(datetime.now())+'.log'))
 
@@ -44,11 +51,18 @@ class SpringerPackage(object):
         #     self.conn.run()
         print >> sys.stderr, "cc"
         self._crawl_springer_and_find_main_xml()
-        #self._crawl_springer_and_find_issue_xml()
-        #self._build_doi_mapping()
 
     def bibupload_it(self):
-        pass
+        self.logger.debug("Preparing bibupload.")
+        fd, name = mkstemp(suffix='.xml', prefix='bibupload_scoap3_', dir=CFG_TMPSHAREDDIR)
+        out = fdopen(fd, 'w')
+        print >> out, "<collection>"
+        for i, path in enumerate(self.found_articles):
+            print >> out, self.get_record(path)
+            print path, i + 1, "out of", len(self.found_articles)
+        print >> out, "</collection>"
+        out.close()
+        task_low_level_submission("bibupload", "Elsevier", "-i", "-r", name)
 
     def _extract_package(self):
         """
@@ -72,6 +86,7 @@ class SpringerPackage(object):
         a main.xml in agiven directory.
         """
         self.found_articles = []
+        #self.found_articles_names = []
         # if not self.path and not self.package_name:
         #     for doc in self.conn.found_articles:
         #         dirname = doc['xml'].rstrip('/main.xml')
@@ -82,18 +97,18 @@ class SpringerPackage(object):
         #             register_exception()
         #             print >> sys.stderr, "ERROR: can't normalize %s: %s" % (dirname, err)
         # else:
+
         def visit(arg, dirname, names):
-            if [filename for filename in names if ".xml.meta" in filename]:
+            files = [filename for filename in names if ".xml.meta" in filename]
+            if files:
                 try:
                     self._normalize_article_dir_with_dtd(dirname)
                     self.found_articles.append(dirname)
+                    #self.found_articles_names.extend(map(partial(join(dirname)), files))
                 except Exception, err:
                     register_exception()
                     print >> sys.stderr, "ERROR: can't normalize %s: %s" % (dirname, err)
         walk(self.path, visit, None)
-
-    def _crawl_springer_and_find_issue_xml(self):
-        pass
 
     def _normalize_article_dir_with_dtd(self, path):
         """
@@ -107,6 +122,7 @@ class SpringerPackage(object):
             return
         # Add looking for a xml.meta file
         # if 'art520' in open(join(path, 'main.xml')).read():
+
         ZipFile(CFG_SPRINGER_AV24_PATH).extractall(path)
         # for filename in listdir(join(path, '')):
         #     print >> sys.stderr, filename
@@ -122,9 +138,160 @@ class SpringerPackage(object):
             self.logger.error("Error in cleaning %s: %s" % (join(path, 'issue.xml'), cmd_err))
             raise ValueError("Error in cleaning %s: %s" % (join(path, 'main.xml'), cmd_err))
 
+    def get_article(self, path):
+        return parse(open(path))
 
-    def _build_doi_mapping(self):
-        pass
+    def get_title(self, xml):
+        try:
+            return get_value_in_tag(xml, "ArticleTitle")
+        except Exception, err:
+            print >> sys.stderr, "Can't find title"
+
+    def get_publication_information(self, xml):
+        doi = self._get_doi(xml)
+        try:
+            return self._dois[doi] + (doi, )
+        except:
+            return ('', '', '', '', '', '', '', doi)
+
+    def _get_doi(self, xml):
+        try:
+            return get_value_in_tag(xml, "ArticleDOI")
+        except Exception, err:
+            print >> sys.stderr, "Can't find doi"
+
+    def get_authors(self, xml):
+        authors = []
+        for author in xml.getElementsByTagName("Author"):
+            tmp = {}
+            surname = get_value_in_tag(author, "FamilyName")
+            if surname:
+                tmp["surname"] = surname
+            given_name = get_value_in_tag(author, "GivenName")
+            if given_name:
+                tmp["given_name"] = " ".join(given_name)
+            # initials = get_value_in_tag(author, "ce:initials")
+            # if initials:
+            #     tmp["initials"] = initials
+            # It's not there
+            # orcid = author.getAttribute('orcid').encode('utf-8')
+            # if orcid:
+            #     tmp["orcid"] = orcid
+            emails = author.getElementsByTagName("Email")
+            for email in emails:
+                if email.getAttribute("type").encode('utf-8') in ('email', ''):
+                    tmp["email"] = xml_to_text(email)
+                    break
+            # cross_refs = author.getElementsByTagName("ce:cross-ref")
+            # if cross_refs:
+            #     tmp["cross_ref"] = []
+            #     for cross_ref in cross_refs:
+            #         tmp["cross_ref"].append(cross_ref.getAttribute("refid").encode('utf-8'))
+            tmp["affiliations_ids"] = author.getAttribute("AffiliationIDS").split().encode('utf-8')
+            authors.append(tmp)
+        affiliations = {}
+        for affiliation in xml.getElementsByTagName("Affiliation"):
+            aff_id = affiliation.getAttribute("ID").encode('utf-8')
+            text = xml_to_text(affiliation)
+            affiliations[aff_id] = text
+        implicit_affilations = True
+        for author in authors:
+            matching_ref = [ref for ref in author.get("affiliations_ids") if ref in affiliations]
+            if matching_ref:
+                implicit_affilations = False
+                author["affiliation"] = []
+                for i in xrange(0, len(matching_ref)):
+                    author["affiliation"].append(affiliations[matching_ref[i]])
+        if implicit_affilations and len(affiliations) > 1:
+            print >> sys.stderr, "Implicit affiliations are used, but there's more than one affiliation: %s" % affiliations
+        if implicit_affilations and len(affiliations) >= 1:
+            for author in authors:
+                author["affiliation"] = []
+                for aff in affiliations.values():
+                    author["affiliation"].append(aff)
+        return authors
+
+    def get_abstract(self, xml):
+        try:
+            return get_value_in_tag(xml, "Abstract")
+        except Exception, err:
+            print >> sys.stderr, "Can't find abstract"
+
+    def get_copyright(self, xml):
+        try:
+            return get_value_in_tag(xml.getElementsByTagName("ArticleCopyright", "CopyrightHolderName"))
+        except Exception, err:
+            print >> sys.stderr, "Can't find copyright"
+
+    def get_keywords(self, xml):
+        try:
+            return [get_value_in_tag(keyword, "ce:text") for keyword in xml.getElementsByTagName("ce:keyword")]
+        except Exception, err:
+            print >> sys.stderr, "Can't find keywords"
+
+    def get_record(self, path):
+        self.logger.info("Creating record: %s" % (path,))
+        xml = self.get_article(join(path, "resolved_main.xml"))
+        rec = {}
+        title = self.get_title(xml)
+        if title:
+            record_add_field(rec, '245', subfields=[('a', title)])
+        record_add_field(rec, '260', subfields=[('c', time.strftime('%Y-%m-%d'))])
+        journal, issn, volume, issue, first_page, last_page, year, doi = self.get_publication_information(xml)
+        if doi:
+            record_add_field(rec, '024', ind1='7', subfields=[('a', doi), ('2', 'DOI')])
+        authors = self.get_authors(xml)
+        first_author = True
+        for author in authors:
+            subfields = [('a', '%s, %s' % (author['surname'], author.get('given_name') or author.get('initials')))]
+            if 'orcid' in author:
+                subfields.append(('j', author['orcid']))
+            if 'affiliation' in author:
+                for aff in author["affiliation"]:
+                    subfields.append(('u', aff))
+            if first_author:
+                record_add_field(rec, '100', subfields=subfields)
+                first_author = False
+            else:
+                record_add_field(rec, '700', subfields=subfields)
+        abstract = self.get_abstract(xml)
+        if abstract:
+            record_add_field(rec, '520', subfields=[('a', abstract)])
+        record_add_field(rec, '540', subfields=[('a', 'CC-BY-3.0'), ('u', 'http://creativecommons.org/licenses/by/3.0/')])
+        copyright = self.get_copyright(xml)
+        if copyright:
+            record_add_field(rec, '542', subfields=[('f', copyright)])
+        keywords = self.get_keywords(xml)
+        if keywords:
+            for keyword in keywords:
+                record_add_field(rec, '653', ind1='1', subfields=[('a', keyword), ('9', 'author')])
+        record_add_field(rec, '773', subfields=[('p', journal), ('v', volume), ('n', issue), ('c', '%s-%s' % (first_page, last_page)), ('y', year)])
+        references = self.get_references(xml)
+        for label, authors, doi, issue, page, title, volume, year in references:
+            subfields = []
+            if doi:
+                subfields.append(('a', doi))
+            for author in authors:
+                subfields.append(('h', author))
+            if issue:
+                subfields.append(('n', issue))
+            if label:
+                subfields.append(('o', label))
+            if page:
+                subfields.append(('p', page))
+            subfields.append(('s', '%s %s (%s) %s' % (title, volume, year, page)))
+            if title:
+                subfields.append(('t', title))
+            if volume:
+                subfields.append(('v', volume))
+            if year:
+                subfields.append(('y', year))
+            if subfields:
+                record_add_field(rec, '999', ind1='C', ind2='5', subfields=subfields)
+        record_add_field(rec, 'FFT', subfields=[('a', join(path, 'main.pdf'))])
+        record_add_field(rec, 'FFT', subfields=[('a', join(path, 'main.xml'))])
+        record_add_field(rec, '980', subfields=[('a', 'SCOAP3')])
+        return record_xml_output(rec)
 
 
 def main():
