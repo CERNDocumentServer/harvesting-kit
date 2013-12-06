a3d9839a75929e96f773aed335db5abbad6d6e74
Samuele Kaplun <samuele.kaplun@cern.ch>
2013-12-06 10:34:49 +0100

    x

--- scoap3utils.py
+++ scoap3utils.py
@@ -25,10 +25,68 @@ from __future__ import division
 
 import sys
 import logging
+import urllib
+import itertools
+import datetime
+from xml.dom.minidom import parse
 
-from invenio.config import CFG_LOGDIR
+from invenio.search_engine import get_collection_reclist
+from invenio.config import CFG_LOGDIR, CFG_CROSSREF_USERNAME, CFG_CROSSREF_PASSWORD, CFG_SITE_NAME
 from os.path import join
 
+CFG_CROSSREF_DOIS_PER_REQUEST = 10
+CFG_CROSSREF_API_URL = "http://doi.crossref.org/search/doi?"
+
+def chunks(l, n):
+    return [l[i:i+n] for i in range(0, len(l), n)]
+
+def init_db():
+    run_sql("""
+CREATE TABLE IF NOT EXISTS bibrec_scoap3check (
+  id_bibrec mediumint(8) unsigned NOT NULL,
+  doi varchar(255) NOT NULL,
+  arxiv varchar(255) NULL default NULL,
+  doi_timestamp datetime NULL default NULL,
+  last_verification datetime NOT NULL,
+  valid_record boolean NOT NULL default 'false',
+  PRIMARY KEY doi (doi),
+  UNIQUE KEY id_bibrec(id_bibrec),
+  KEY arxiv(arxiv),
+  KEY doi_timestamp(doi_timestamp),
+  KEY last_verification(last_verification),
+  KEY valid_record(valid_record)
+) ENGINE=MyISAM;""")
+
+def get_all_recids_to_check():
+    all_recids = get_collection_reclist(CFG_SITE_NAME)
+    last_verification = run_sql("SELECT MAX(last_verification) FROM bibrec_scoap3check")[0][0] or datetime.datetime(1970, 1, 1, 0, 0)
+    verified_recids = intbitset(run_sql("SELECT id_bibrec FROM bibrec_scoap3check WHERE doi_timestamp IS NOT NULL AND arxiv IS NOT NULL AND valid_record"))
+    unverified_recids = all_recids - verified_recids
+    modified_recids = all_recids & run_sql("SELECT bibrec.id FROM bibrec, bibrec_scoap3check WHERE bibrec.id=id_bibrec AND modification_date>last_verification")
+    return new_recids | modified_recids
+
+def get_all_info_from_recid(recid):
+    
+
+
+def crossref_checker(dois, username=CFG_CROSSREF_USERNAME, password=CFG_CROSSREF_PASSWORD):
+    """
+    Given a list of DOIs send a batch request of up to CFG_CROSSREF_DOIS_PER_REQUEST
+    and return all information
+    """
+    ret = dict([(doi, None) for doi in dois])
+    for chunk in chunks(dois, CFG_CROSSREF_DOIS_PER_REQUEST):
+        query = [("pid", "%s:%s" % (username, password)), ("format", "info"), ("doi", chunk)]
+        results = parse(urllib.urlopen(CFG_CROSSREF_API_URL + urllib.urlencode(query, doseq=True)))
+        for result in results.getElementsByTagName("crossref-metadata"):
+            doi = get_value_in_tag(result, 'doi')
+            if doi not in dois:
+                raise StandardError("CrossRef returned information for a DOI that was not asked for!? %s" % doi)
+            for crm_item in result.getElementsByTagName("crm-item"):
+                if crm_item.getAttribute('name') == u'deposit-timestamp':
+                    ret[doi] = datetime.datetime.strptime(xml_to_text(crm_item), "%Y%m%d%H%M%S%f")
+                    break
+    return ret
 
 def lock_issue():
     """
