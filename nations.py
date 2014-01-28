dde243b680723110a5d039b8dbe5f426690a3111
Samuele Kaplun <samuele.kaplun@cern.ch>
2014-01-28 14:44:07 +0100

    Amending Export and fixing bug

--- nations.py
+++ nations.py
@@ -145,11 +145,11 @@ def articles(req, i):
     return page(req=req, title=page_title, body=body)
 
 def csv(req):
-    req.content_type = 'text/csv'
+    req.content_type = 'text/csv; charset=utf-8'
     req.headers_out['content-disposition'] = 'attachment; filename=scoap3.csv'
     header = ','.join(['Nation'] + [get_coll_i18nname(journal) for journal in CFG_JOURNALS])
     print >> req, header
     for nation_tuple in _CFG_NATION_MAP:
         query = _build_query(nation_tuple)
-        line = ','.join([nation_tuple[0]] + [str(len(perform_request_search(p=query, cc=journal, of='intbitset')))])
+        line = ','.join([nation_tuple[0]] + [str(len(perform_request_search(p=query, cc=journal, of='intbitset'))) for journal in CFG_JOURNALS])
         print >> req, line
