671a62548c26cabc9a77e180aebcdf96c288ac22
Georgios Papoutsakis <georgios.papoutsakis@cern.ch>
2014-07-18 15:54:46 +0200

    utils: collapse_initials fix
    * Fix to handle more than one spaces between initials.
    
    Signed-of-by: Georgios Papoutsakis <georgios.papoutsakis@cern.ch>

--- harvestingkit/utils.py
+++ harvestingkit/utils.py
@@ -81,7 +81,7 @@ def collapse_initials(name):
     """ Removes the space between initials.
         eg T. A. --> T.A."""
     if len(name.split()) > 1:
-        name = re.sub(r'([A-Z]\.) (?=[A-Z]\.)', r'\1', name)
+        name = re.sub(r'([A-Z]\.) +(?=[A-Z]\.)', r'\1', name)
     return name
 
 
