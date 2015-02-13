809c865cdbbc582dc46dfdd4c6d8f09b77b30a64
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-02-13 10:53:18 +0100

    setup: release 0.4.0
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-##
-## This file is part of Harvesting Kit.
-## Copyright (C) 2013, 2014 CERN.
-##
-## Harvesting Kit is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License as
-## published by the Free Software Foundation; either version 2 of the
-## License, or (at your option) any later version.
-##
-## Harvesting Kit is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with Harvesting Kit; if not, write to the Free Software Foundation, Inc.,
-## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+# This file is part of Harvesting Kit.
+# Copyright (C) 2013, 2014, 2015 CERN.
+#
+# Harvesting Kit is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Harvesting Kit is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Harvesting Kit; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """Kit of tools to convert publisher XML (NLM/JATS) to MARCXML."""
 
@@ -27,7 +27,7 @@ with open('requirements.txt', 'r') as req:
 
 setup(
     name="HarvestingKit",
-    version="0.3.0",
+    version="0.4.0",
     packages=find_packages(),
     package_data={'': ['data/*.xml']},
     data_files=[("dtds", ["dtds/ja5_art501.zip",
