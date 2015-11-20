cc25ad661610a387baeccb0cce917d0dd1a537bb
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-11-20 14:35:33 +0100

    setup: release 0.6.5
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="HarvestingKit",
-    version="0.6.4",
+    version="0.6.5",
     packages=find_packages(),
     package_data={'': ['data/*.xml']},
     data_files=[("dtds", ["dtds/ja5_art501.zip",
