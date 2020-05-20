8bdc57e928295b4e26e497bee8c32291d243b2d4
Micha Moskovic <michamos@gmail.com>
2020-05-20 15:15:28 +0200

    setup: release v0.6.21
    Signed-off-by: Micha Moskovic <michamos@gmail.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="HarvestingKit",
-    version="0.6.20",
+    version="0.6.21",
     packages=find_packages(),
     package_data={
         '': ['data/*.xml'],
