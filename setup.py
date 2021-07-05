53937c413fce1992968a47c5e758037b1b065f5a
jrcastro2 <jrcastro9515@gmail.com>
2021-07-05 18:01:20 +0200

    setup: release v0.6.23

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="HarvestingKit",
-    version="0.6.22",
+    version="0.6.23",
     packages=find_packages(),
     package_data={
         '': ['data/*.xml'],
