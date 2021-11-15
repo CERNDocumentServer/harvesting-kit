7ec671c5f97ac7e758d3d30cbd61197f45e753e1
Nicola Tarocco <ntarocco@gmail.com>
2021-11-15 19:09:00 +0100

    setup: release v0.6.24

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="HarvestingKit",
-    version="0.6.23",
+    version="0.6.24",
     packages=find_packages(),
     package_data={
         '': ['data/*.xml'],
